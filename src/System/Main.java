6cbbd5adeaddc2253893471e12f8fd4c3e55e50a
tk-wong <108210425+tk-wong@users.noreply.github.com>
2024-09-29 21:22:22 +0800

    testing

--- src/System/Main.java
+++ src/System/Main.java
@@ -3,9 +3,9 @@ package System;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
-		System.out.println("testing");
 		// test
 		// more test 2
 		// more test 3
+		// testing: tk-wong
 	}
 }
